0e42e8fa1c4c8a680b0aeda4f0cd4cf92c8ddab9
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-04-26 18:22:33 +0200

    creating aruco markers

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,31 @@
-#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/aruco.hpp>
+#include <opencv2/calib3d.hpp>
+
 #include <thread>
 #include <chrono>
+#include <sstream>
+#include <fstream>
+#include <iostream>
 
-#include "kinect.h"
+void createArucoMarkers(){
+    cv::Mat outputMarker;
+    cv::Ptr<cv::aruco::Dictionary> markerDictionary = cv::aruco::getPredefinedDictionary(cv::aruco::PREDEFINED_DICTIONARY_NAME::DICT_4X4_50);
 
-const int fps = 20;
+    for (int i = 0; i < 50; i++){
+        cv::aruco::drawMarker(markerDictionary, i, 500, outputMarker, 1);
+        std::ostringstream convert;
+        std::string  imageName = "4x4Marker_";
+        convert << imageName << i << ".jpg";
+        cv::imwrite(convert.str(), outputMarker);
+    }
+}
+
 int main(int argc, char* argv[])
 {
-    /** initialize kinect */
-    std::shared_ptr<Kinect> sptr_kinect(new Kinect);
-    int rgbWidth = k4a_image_get_width_pixels(sptr_kinect->m_rgbImage);
-    int rgbHeight = k4a_image_get_height_pixels(sptr_kinect->m_rgbImage);
-
-
-    while (true){
-        /** get next frame from kinect */
-        sptr_kinect->getFrame(RGB_TO_DEPTH);
-
-        /** get image from kinect */
-        uint8_t* color_image_data = k4a_image_get_buffer(sptr_kinect->m_rgbImage);
-
-        /** release resources */
-        sptr_kinect->release();
-
-        /** cast to cv::Mat */
-        cv::Mat frame = cv::Mat(rgbHeight, rgbWidth, CV_8UC4, (void*)color_image_data, cv::Mat::AUTO_STEP);
-
-        cv::imshow("kinect", frame);
-        if(cv::waitKey(1000/fps) >=0){
-            break;
-        }
-        std::this_thread::sleep_for(std::chrono::milliseconds(2));
-    }
+    createArucoMarkers();
     return 0;
 }
