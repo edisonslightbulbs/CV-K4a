87bd119273e0a27a22408f855ee2d25a2ab41dfb
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-04-26 10:10:47 +0200

    using cv::split

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,23 @@
 #include <opencv2/opencv.hpp>
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
 #include "kinect.h"
 
 int main(int argc, char* argv[])
 {
     /** initialize kinect */
-    std::shared_ptr<Kinect> sptr_kinect(new Kinect);
-
     const std::string IMAGE = "./output/scene.png";
+    std::shared_ptr<Kinect> sptr_kinect(new Kinect);
     int rgbWidth = k4a_image_get_width_pixels(sptr_kinect->m_rgbImage);
     int rgbHeight = k4a_image_get_height_pixels(sptr_kinect->m_rgbImage);
 
     /** get image from kinect */
     uint8_t* color_image_data = k4a_image_get_buffer(sptr_kinect->m_rgbImage);
+
+    /** release resources */
+    sptr_kinect->release();
 
     /** cast to cv::Mat */
     cv::Mat img = cv::Mat(rgbHeight, rgbWidth, CV_8UC4, (void*)color_image_data, cv::Mat::AUTO_STEP);
@@ -20,38 +25,19 @@ int main(int argc, char* argv[])
     /** write image */
     cv::imwrite(IMAGE, img);
 
-    /** load gray scale */
-    cv::Mat gImgFile = cv::imread(IMAGE, cv::IMREAD_GRAYSCALE);
-    cv::Mat modifiedGray = cv::imread(IMAGE, cv::IMREAD_GRAYSCALE);
+    /** load color image */
+    cv::Mat colorImage = cv::imread(IMAGE, cv::IMREAD_COLOR);
+    // *img from kinect needs to be cast into a cv color image, i.e.,
+    // into a 3 channel image first before using split. Not casting it
+    // before using split will cause a seg fault.
 
-    /** modifying gray-scale image */
-    for(int r = 0; r < gImgFile.rows; r++){
-        for(int c = 0; c < gImgFile.cols; c++ ){
-            modifiedGray.at<uint8_t>(r, c) = modifiedGray.at<uint8_t>(r, c) * 0.5;
-        }
-    }
+    /** split R G B channels */
+    cv::Mat rgbChannel[3];
+    cv::split(colorImage, rgbChannel);
 
-    /** load color image*/
-    cv::Mat imgFile = cv::imread(IMAGE, cv::IMREAD_COLOR);
-    cv::Mat modifiedColor = cv::imread(IMAGE, cv::IMREAD_COLOR);
-
-    /** modifying color image */
-    for(int r = 0; r < imgFile.rows; r++){
-        for(int c = 0; c < imgFile.cols; c++ ){
-            modifiedColor.at<cv::Vec3b>(r, c)[0] = modifiedColor.at<cv::Vec3b>(r, c)[0] * 0;
-        }
-    }
-
-
-    /** show images */
-    cv::imshow("gray", gImgFile);
-    cv::imshow("modifiedGray", modifiedGray);
-
-    cv::imshow("color", imgFile);
-    cv::imshow("modifiedColor", modifiedColor);
-
+    cv::imshow("color", img);
+    cv::imshow("blue", rgbChannel[0]);
+    cv::imshow("green", rgbChannel[1]);
+    cv::imshow("red", rgbChannel[2]);
     cv::waitKey();
-
-    /** release resources */
-    sptr_kinect->release();
 }
