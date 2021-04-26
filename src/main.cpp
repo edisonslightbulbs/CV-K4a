990e7bfc20d9786d5a9263ff93fa431b461e51a2
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-04-26 15:53:49 +0200

    streaming with the kinect

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,36 @@
 #include <opencv2/opencv.hpp>
+#include <thread>
+#include <chrono>
 
 #include "kinect.h"
 
-void createGaussian(cv::Size& size, cv:: Mat& output, int uX, int uY, float sigmaX, float sigmaY, float amplitude = 1.0f){
-    cv::Mat temp = cv::Mat(size, CV_32F);
-    for (int r = 0; r < size.height; r++){
-        for(int c = 0; c < size.width; c++){
-            float x = (float)((c - uX) * (c -uX)) / (2.0f * sigmaX * sigmaX);
-            float y = (float)((c - uY) * (c -uY)) / (2.0f * sigmaY * sigmaY);
-            float value  = amplitude * exp(-(x + y));
-            temp.at<float>(r, c) = value;
-        }
-        cv::normalize(temp, temp, 0.0f, 1.0f, cv::NORM_MINMAX);
-        output = temp;
-    }
-}
-
-
+const int fps = 20;
 int main(int argc, char* argv[])
 {
     /** initialize kinect */
-    const std::string IMAGE = "./output/scene.png";
     std::shared_ptr<Kinect> sptr_kinect(new Kinect);
     int rgbWidth = k4a_image_get_width_pixels(sptr_kinect->m_rgbImage);
     int rgbHeight = k4a_image_get_height_pixels(sptr_kinect->m_rgbImage);
 
-    /** get image from kinect */
-    uint8_t* color_image_data = k4a_image_get_buffer(sptr_kinect->m_rgbImage);
 
-    /** release resources */
-    sptr_kinect->release();
+    while (true){
+        /** get next frame from kinect */
+        sptr_kinect->getFrame(RGB_TO_DEPTH);
 
-    /** cast to cv::Mat */
-    cv::Mat img = cv::Mat(rgbHeight, rgbWidth, CV_8UC4, (void*)color_image_data, cv::Mat::AUTO_STEP);
+        /** get image from kinect */
+        uint8_t* color_image_data = k4a_image_get_buffer(sptr_kinect->m_rgbImage);
 
-    /** write image */
-    cv::imwrite(IMAGE, img);
+        /** release resources */
+        sptr_kinect->release();
 
-    /** load grey-scale image */
-    cv::Mat grayImage = cv::imread(IMAGE, cv::IMREAD_GRAYSCALE);
-    // *img from kinect needs to be cast into a cv color image, i.e.,
-    // into a 3 channel image first before using split. Not casting it
-    // before using split will cause a seg fault.
+        /** cast to cv::Mat */
+        cv::Mat frame = cv::Mat(rgbHeight, rgbWidth, CV_8UC4, (void*)color_image_data, cv::Mat::AUTO_STEP);
 
-    //  actual gaussian computation starts here:
-    //
-    cv::Mat output;
-    cv::Size s = cv::Size(256, 256);
-
-    createGaussian(s, output, 256/2, 256/2, 10,10, 1.0f);
-    cv::imshow("gaussian", output);
-
-    cv::waitKey();
+        cv::imshow("kinect", frame);
+        if(cv::waitKey(1000/fps) >=0){
+            break;
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(2));
+    }
+    return 0;
 }
