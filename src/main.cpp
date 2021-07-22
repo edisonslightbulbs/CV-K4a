010ff48ee7c467885d808b4d20a16cb1f99c36c3
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-07-22 13:22:17 +0200

    testing homography example

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
+// #include <iostream>
+//
+// int main()
+// {
+//     std::cout << "-- see in examples directory for the code " << std::endl;
+//     std::cout << "-- see in build/bin directory for the binaries " <<
+//     std::endl;
+// }
+
+#include "homography.h"
+#include <opencv2/opencv.hpp>
 
 int main()
 {
-    std::cout << "-- see in examples directory for the code " << std::endl;
-    std::cout << "-- see in build/bin directory for the binaries " << std::endl;
+    cv::Mat backgroundImg, foregroundImg;
+    backgroundImg = cv::imread("resources/background.png", cv::IMREAD_COLOR);
+    foregroundImg = cv::imread("resources/foreground.png", cv::IMREAD_COLOR);
+    homography::initialize(backgroundImg, foregroundImg);
 }
