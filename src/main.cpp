ea72a6931feca86ad18838c26742b37a638465e5
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-08-04 15:00:00 +0200

    migrating Camera class implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
-#include <chrono>
 #include <opencv2/opencv.hpp>
-#include <thread>
 
 #include "camera.h"
+#include "file.h"
 #include "kinect.h"
-#include "parameters.h"
 #include "usage.h"
+
+bool calibrateCamera(
+    Camera& camera, const cv::Size& dChessboard, const std::string& file)
+{
+    bool done = false;
+    if (camera.m_images.size() > 15) {
+        usage::prompt(CALIBRATING);
+        camera.calibrate(dChessboard, chessboard::R_BLOCK_WIDTH);
+        usage::prompt(SAVING_PARAMETERS);
+        parameters::write(file, camera.m_matrix, camera.m_K);
+        done = true;
+    } else {
+        usage::prompt(MORE_IMAGES_REQUIRED);
+    }
+    return done;
+}
 
 cv::Mat grabFrame(std::shared_ptr<Kinect>& sptr_kinect)
 {
@@ -19,144 +33,33 @@ cv::Mat grabFrame(std::shared_ptr<Kinect>& sptr_kinect)
     return cv::Mat(h, w, CV_8UC4, (void*)data, cv::Mat::AUTO_STEP).clone();
 }
 
-#ifndef CAMERA_H
-#define CAMERA_H
-class Camera {
-public:
-    std::vector<cv::Mat> m_R;
-    std::vector<cv::Mat> m_t;
-
-    cv::Mat m_K;
-    cv::Mat m_matrix;
-
-    std::vector<cv::Point2f> m_cameraSpaceCorners;
-    std::vector<std::vector<cv::Point3f>> m_worldSpaceCorners;
-    std::vector<std::vector<cv::Point2f>> m_imageSpaceCorners;
-
-    void findCameraSpaceCorners(std::vector<cv::Mat>& images,
-        std::vector<std::vector<cv::Point2f>>& corners)
-    {
-        for (auto& image : images) {
-            std::vector<cv::Point2f> corner;
-            bool found = cv::findChessboardCorners(image, cv::Size(9, 6),
-                corner,
-                cv::CALIB_CB_ADAPTIVE_THRESH | cv::CALIB_CB_NORMALIZE_IMAGE);
-
-            if (found) {
-                corners.emplace_back(corner);
-            }
-        }
-    }
-
-    /**
-     * Evaluates the R (rotation) , t (translation), camera matrix,
-     * & K (distance coefficients).
-     *
-     * @param images
-     *   Images of chessboard captured by the camera.
-     * @param boardSize
-     *   Dimensions of the chessboard.
-     * @param blockWidth
-     *   Width of a single chessboard block
-     */
-    void calibrate(std::vector<cv::Mat> images, const cv::Size& boardSize,
-        float blockWidth)
-    {
-        findCameraSpaceCorners(images, m_imageSpaceCorners);
-        chessboard::findWorldSpaceCorners(
-            boardSize, blockWidth, m_worldSpaceCorners[0]);
-
-        m_worldSpaceCorners.resize(
-            m_imageSpaceCorners.size(), m_worldSpaceCorners[0]);
-
-        cv::calibrateCamera(m_worldSpaceCorners, m_imageSpaceCorners, boardSize,
-            m_matrix, m_K, m_R, m_t);
-    }
-
-    Camera()
-    {
-        m_K = cv::Mat::zeros(8, 1, CV_64F);
-        m_matrix = cv::Mat::eye(3, 3, CV_64F);
-        m_worldSpaceCorners = std::vector<std::vector<cv::Point3f>>(1);
-    };
-};
-#endif // CAMERA_H
-
-bool calibrateCamera(Camera& camera, std::vector<cv::Mat>& chessboardImages,
-    const cv::Size& dChessboard)
-{
-    bool done = false;
-    if (chessboardImages.size() > 15) {
-        usage::prompt(COMPUTING_CALIBRATION_PARAMETERS);
-        camera.calibrate(
-            chessboardImages, dChessboard, chessboard::R_BLOCK_WIDTH);
-        usage::prompt(WRITING_CALIBRATION_PARAMETERS);
-        parameters::write(
-            "./output/calibration/camera.txt", camera.m_matrix, camera.m_K);
-        std::this_thread::sleep_for(std::chrono::seconds(5));
-        done = true;
-    } else {
-        usage::prompt(MORE_CHESSBOARD_IMAGES_REQUIRED);
-    }
-    return done;
-}
-
-void getChessboardImage(
-    const bool& pass, cv::Mat& src, std::vector<cv::Mat>& chessboardImages)
-{
-    if (pass) {
-        cv::Mat temp;
-        src.copyTo(temp);
-        chessboardImages.emplace_back(temp);
-        int imgCount = (int)chessboardImages.size();
-        std::cout << "-- # images: " << imgCount << std::endl;
-    }
-}
-
-bool overlayCorners(const cv::Mat& src, cv::Mat dst,
-    const cv::Size& dChessboard, const std::string& window)
-{
-    std::vector<cv::Point2f> corners;
-    bool found = cv::findChessboardCorners(src, dChessboard, corners,
-        cv::CALIB_CB_ADAPTIVE_THRESH | cv::CALIB_CB_NORMALIZE_IMAGE);
-    if (found) {
-        src.copyTo(dst);
-        cv::drawChessboardCorners(dst, dChessboard, corners, found);
-        cv::imshow(window, dst);
-    } else {
-        cv::imshow(window, src);
-    }
-    return found;
-}
-
 int main()
 {
-    // initialize camera, window, and images
+    // initialize camera, image frames, and  window
     Camera camera;
+    cv::Mat src, dst;
     std::shared_ptr<Kinect> sptr_kinect(new Kinect);
 
-    const std::string WINDOW = "calibration window";
-    cv::namedWindow(WINDOW, cv::WINDOW_AUTOSIZE);
-
-    cv::Mat src, dst;
-    std::vector<cv::Mat> chessboardImages;
+    std::string window = "calibration window";
+    cv::namedWindow(window, cv::WINDOW_AUTOSIZE);
     const cv::Size dChessboard = cv::Size(9, 6);
 
     // start calibration
     bool done = false;
     usage::prompt(USAGE);
+    std::string file = "./output/calibration/camera.txt";
 
     while (!done) {
         src = grabFrame(sptr_kinect);
-        bool pass = overlayCorners(src, dst, dChessboard, WINDOW);
+        bool pass = chessboard::overlay(src, dst, dChessboard, window);
 
         int key = cv::waitKey(30);
         switch (key) {
-        case ENTER_KEY: // capture chessboard images
-            getChessboardImage(pass, src, chessboardImages);
+        case ENTER_KEY: // capture images
+            chessboard::capture(pass, src, camera.m_images);
             break;
         case ESCAPE_KEY: // start calibration
-            done = calibrateCamera(camera, chessboardImages, dChessboard);
+            done = calibrateCamera(camera, dChessboard, file);
         default:
             break;
         }
