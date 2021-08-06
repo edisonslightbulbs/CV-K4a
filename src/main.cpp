72f66db67fbbd9609f182aa0e7d02ee8eee71622
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-08-06 08:29:11 +0200

    optimal extraction of flux AOE

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,22 @@ cv::Mat grabFrame(std::shared_ptr<Kinect>& sptr_kinect)
 }
 #endif
 
+void saturate(const cv::Mat& src, cv::Mat& dst)
+{
+    int beta = 100;     // brightness | range 1 - 100
+    double alpha = 3.0; // contrast | range 1.0 - 3.0]
+
+    dst = cv::Mat::zeros(src.size(), src.type());
+    for (int y = 0; y < src.rows; y++) {
+        for (int x = 0; x < src.cols; x++) {
+            for (int c = 0; c < src.channels(); c++) {
+                dst.at<cv::Vec3b>(y, x)[c] = cv::saturate_cast<uchar>(
+                    alpha * src.at<cv::Vec3b>(y, x)[c] + beta);
+            }
+        }
+    }
+}
+
 int main()
 {
     std::vector<cv::Mat> scene(2);
@@ -49,16 +65,23 @@ int main()
     cv::Mat colorDiff, gray_0, gray_1, grayDiff, sharpGray;
     cv::cvtColor(scene[0], gray_0, cv::COLOR_BGR2GRAY);
     cv::cvtColor(scene[1], gray_1, cv::COLOR_BGR2GRAY);
+
+    // cv::absdiff(scene[1], scene[0], colorDiff);
+    // cv::absdiff(gray_1, gray_0, grayDiff);
     colorDiff = scene[0] - scene[1];
     grayDiff = gray_0 - gray_1;
 
     // sharpen gray diff
-    cv::equalizeHist(grayDiff, sharpGray);
+    // cv::equalizeHist(grayDiff, sharpGray); // <- iffy output
+    cv::Mat saturated;
+    saturate(colorDiff, saturated);
+    cv::cvtColor(saturated, sharpGray, cv::COLOR_BGR2GRAY);
 
     // remove noise using smoothing
     cv::Mat blurred, thresholded, roi;
     cv::Size dBlur = cv::Size(33, 33);
-    cv::GaussianBlur(grayDiff, blurred, dBlur, 0);
+    // cv::GaussianBlur(grayDiff, blurred, dBlur, 0);
+    cv::GaussianBlur(sharpGray, blurred, dBlur, 0);
 
     // threshold to extract flux
     cv::threshold(
@@ -71,7 +94,9 @@ int main()
     // show results (flux AOE)
     cv::imshow("Color difference", colorDiff);
     cv::imshow("Gray difference", grayDiff);
-    cv::imshow("Sharp gray diff", sharpGray);
+    cv::imshow("Saturated", saturated);
+    cv::imshow("Sharpened gray diff", sharpGray);
+    cv::imshow("Blurred gray diff", blurred);
     cv::imshow("OTSU threshold", thresholded);
     cv::imshow("Region of interest", roi);
 
