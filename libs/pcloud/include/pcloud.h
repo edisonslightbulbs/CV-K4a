5bc1beaf8ecf5a0197ad663f6e46b986357e943b
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-08-07 07:39:48 +0200

    (+) Point class

--- libs/pcloud/include/pcloud.h
+++ libs/pcloud/include/pcloud.h
@@ -1,19 +1,17 @@
 #ifndef POINT_CLOUD_H
 #define POINT_CLOUD_H
 
+#include "point.h"
 #include <string>
 #include <vector>
 
-struct t_rgbd {
-    int16_t xyz[3];
-    uint8_t rgb[3];
-};
-
 namespace pcloud {
-std::vector<t_rgbd> retrieve(const int& w, const int& h, const int16_t* pCloudData, const uint8_t* rgbData);
+std::vector<Point> build(
+    const int& w, const int& h, const int16_t* pCloudData, const uint8_t* bgra);
 
-void write(const int& w, const int& h, const int16_t* pCloudData, const uint8_t* rgbData, const std::string& file);
+void write(const int& w, const int& h, const int16_t* pCloudData,
+    const uint8_t* rgbData, const std::string& file);
 
-    void write(const std::vector<t_rgbd> &pCloud, const std::string &file);
+void write(const std::vector<Point>& pCloud, const std::string& file);
 }
 #endif // POINT_CLOUD_H
