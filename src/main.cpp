846579fa02701b21b7c4eef32367524e6365528b
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-08-10 10:40:48 +0200

    updating submodule refs

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 #include "scene.h"
-#include <iostream>
-#include <opencv2/opencv.hpp>
 
-int main(){
+int main()
+{
     // initialize camera
     std::shared_ptr<Kinect> sptr_kinect(new Kinect);
     cv::Mat frame = scene::grabFrame(sptr_kinect);
