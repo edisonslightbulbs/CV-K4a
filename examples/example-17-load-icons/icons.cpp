3aa107dadb43bfe4520561f959c3a95a7cc85260
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-08-11 09:28:42 +0200

    experiment session: one

--- examples/example-17-load-icons/icons.cpp
+++ examples/example-17-load-icons/icons.cpp
@@ -1,33 +1,10 @@
 #include <string>
-#include <thread>
-#include <chrono>
 
 #include <opencv2/opencv.hpp>
 #include "icon.h"
-#include "kinect.h"
-
-cv::Mat grabFrame(std::shared_ptr<Kinect>& sptr_kinect)
-{
-    sptr_kinect->capture();
-    sptr_kinect->imgCapture();
-    uint8_t* rgbData = k4a_image_get_buffer(sptr_kinect->m_img);
-    int w = k4a_image_get_width_pixels(sptr_kinect->m_img);
-    int h = k4a_image_get_height_pixels(sptr_kinect->m_img);
-
-    cv::Mat frame
-            = cv::Mat(h, w, CV_8UC4, (void*)rgbData, cv::Mat::AUTO_STEP).clone();
-
-    sptr_kinect->releaseK4aCapture();
-    sptr_kinect->releaseK4aImages();
-
-    return frame;
-}
 
 int main()
 {
-    // initialize kinect
-    std::shared_ptr<Kinect> sptr_kinect(new Kinect);
-
     // create full screen window
     const std::string window = "scene";
     cv::namedWindow(window, cv::WINDOW_NORMAL);
@@ -78,15 +55,11 @@ int main()
 
     // overlay foreground on background @ roi
     foreground_1.copyTo(backgroundRoi_1);
-    //foreground_2.copyTo(backgroundRoi_2);
+    foreground_2.copyTo(backgroundRoi_2);
     foreground_3.copyTo(backgroundRoi_3);
 
     // show window at desired location
     cv::imshow(window, background);
     cv::moveWindow(window, 3000, 0);
     cv::waitKey();
-
-    // std::this_thread::sleep_for(std::chrono::seconds(2));
-    // cv::Mat sceneImg = grabFrame(sptr_kinect);
-    // cv::imwrite("./output/scene/icons.png", sceneImg);
 }
