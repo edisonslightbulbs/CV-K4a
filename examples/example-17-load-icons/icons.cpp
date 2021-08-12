1f55d6be839206698cc86ed94b84f02fe236e396
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-08-12 14:52:31 +0200

    minor refactoring

--- examples/example-17-load-icons/icons.cpp
+++ examples/example-17-load-icons/icons.cpp
@@ -1,14 +1,15 @@
 #include <string>
 
-#include <opencv2/opencv.hpp>
 #include "icon.h"
+#include <opencv2/opencv.hpp>
 
 int main()
 {
     // create full screen window
     const std::string window = "scene";
     cv::namedWindow(window, cv::WINDOW_NORMAL);
-    cv::setWindowProperty(window, cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
+    cv::setWindowProperty(
+        window, cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
 
     // create background image
     const int h = 768;
@@ -21,7 +22,7 @@ int main()
     cv::Mat foreground_3 = icon::load("./resources/icons/facebook.png");
 
     // saturate foreground image
-    int beta = 0;     // brightness | range 1 - 100
+    int beta = 0;       // brightness | range 1 - 100
     double alpha = 3.0; //   contrast | range 1.0 - 3.0
     icon::saturate(foreground_1, beta, alpha);
     icon::saturate(foreground_2, beta, alpha);
@@ -36,8 +37,8 @@ int main()
 
     // initialize starting position for drawing
     // foreground image on background image
-    int xMin = background.cols/2;
-    int yMin = background.rows/2;
+    int xMin = background.cols / 2;
+    int yMin = background.rows / 2;
 
     // get width and height of the foreground image
     int width = foreground_1.cols;
@@ -45,8 +46,10 @@ int main()
 
     // create roi using starting position and size of foreground
     cv::Rect roi_1 = cv::Rect(xMin, yMin, width, height);
-    cv::Rect roi_2 = cv::Rect(xMin + (xMin/2), yMin + (yMin/2), width, height);
-    cv::Rect roi_3 = cv::Rect(xMin - (xMin/2), yMin - (yMin/2), width, height);
+    cv::Rect roi_2
+        = cv::Rect(xMin + (xMin / 2), yMin + (yMin / 2), width, height);
+    cv::Rect roi_3
+        = cv::Rect(xMin - (xMin / 2), yMin - (yMin / 2), width, height);
 
     // get roi from background image
     cv::Mat backgroundRoi_1 = background(roi_1);
@@ -55,7 +58,7 @@ int main()
 
     // overlay foreground on background @ roi
     foreground_1.copyTo(backgroundRoi_1);
-    foreground_2.copyTo(backgroundRoi_2);
+    // foreground_2.copyTo(backgroundRoi_2);
     foreground_3.copyTo(backgroundRoi_3);
 
     // show window at desired location
